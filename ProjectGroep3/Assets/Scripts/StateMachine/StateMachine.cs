4a60101a48b986db581c005d3e6810273cf81455
schlorp <70897090+schlorp@users.noreply.github.com>
2024-02-26 10:35:38 +0100

    singletonBehaviour
    added: singletonBehaviour
    changed: statemachine

--- ProjectGroep3/Assets/Scripts/StateMachine/StateMachine.cs
+++ ProjectGroep3/Assets/Scripts/StateMachine/StateMachine.cs
@@ -2,21 +2,28 @@ using System;
 using UnityEngine;
 using Utilities;
 
-public class StateMachine : MonoBehaviour
+namespace StateMachineNameSpace
 {
-    //--------------------Private--------------------//
-    [SerializeField]
-    private StateMachineState _currentState;
-
-    //--------------------Public--------------------//
-    public StateMachineState _CurrentState => _currentState;
-
-    public Action<StateMachineState> OnStateSet;
-
-    //--------------------Functions--------------------//
-    public void SetState(StateMachineState stateToSet)
+    public class StateMachine : SingletonBehaviour<StateMachine>
     {
-        _currentState = stateToSet;
-        OnStateSet.Invoke(_currentState);
+        //--------------------Private--------------------//
+        [SerializeField]
+        private StateMachineState _currentState;
+
+        //--------------------Public--------------------//
+        public StateMachineState _CurrentState => _currentState;
+
+        public Action<StateMachineState> OnStateSet;
+
+        //--------------------Functions--------------------//
+        /// <summary>
+        /// Function to set the state of the statemachine, fires a event
+        /// </summary>
+        /// <param name="stateToSet">The state to set the state machine to</param>
+        public void SetState(StateMachineState stateToSet)
+        {
+            _currentState = stateToSet;
+            OnStateSet.Invoke(_currentState);
+        }
     }
 }
