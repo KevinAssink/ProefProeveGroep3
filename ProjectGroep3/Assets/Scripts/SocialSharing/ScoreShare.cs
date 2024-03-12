74f1bb4111cad90625c46e14e938055b9bc69129
schlorp <70897090+schlorp@users.noreply.github.com>
2024-03-12 13:19:49 +0100

    changed share message
    changed: ScoreShare

--- ProjectGroep3/Assets/Scripts/SocialSharing/ScoreShare.cs
+++ ProjectGroep3/Assets/Scripts/SocialSharing/ScoreShare.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
 namespace ScoreSharing
@@ -12,23 +10,14 @@ namespace ScoreSharing
             StartCoroutine(TakeScreenshotAndShare());
         }
 
+        //als we een download link hebben, voeg die toe
         private IEnumerator TakeScreenshotAndShare()
         {
             yield return new WaitForEndOfFrame();
-            Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-            ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-            ss.Apply();
-            string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
-            File.WriteAllBytes(filePath, ss.EncodeToPNG());
-            // To avoid memory leaks
-            Destroy(ss);
-            new NativeShare().AddFile(filePath)
-                .SetSubject("Subject goes here").SetText("Hello world!").SetUrl("https://github.com/yasirkula/UnityNativeShare")
+            new NativeShare()
+                .SetSubject("HighScore").SetText("I beat Synthwave Outlaws with a highscore of: " + PlayerPrefs.GetFloat("HighScore") + ". Can you Beat me?!?!")
                 .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
                 .Share();
-            // Share on WhatsApp only, if installed (Android only)
-            //if( NativeShare.TargetExists( "com.whatsapp" ) )
-            //	new NativeShare().AddFile( filePath ).AddTarget( "com.whatsapp" ).Share();
         }
     }
 }
